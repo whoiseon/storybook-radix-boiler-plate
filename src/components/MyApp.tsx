b0f950da39d6b0593ea48f903530cdb4802b0d5f
whoiseon <whois__@naver.com>
2023-12-01 18:55:21 +0900

    feat: initialized radix ui

---
+++ src/components/MyApp.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import {Button, Flex, Text} from "@radix-ui/themes";
+
+function MyApp() {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+      <Button>Let's go</Button>
+    </Flex>
+  );
+}
+
+export default MyApp;
